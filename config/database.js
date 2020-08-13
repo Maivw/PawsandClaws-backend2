aca30faddbf9a33be2aa0b4739c8a086324ee04e
tran <fidtran@gmail.com>
2020-08-13 01:38:25 -0400

    deploy

--- config/database.js
+++ config/database.js
@@ -7,14 +7,16 @@ const database = db.database;
 const host = db.host;
 
 module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: "postgres",
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-  },
+	development: {
+		username,
+		password,
+		database,
+		host,
+		dialect: "postgres",
+	},
+	production: {
+		use_env_variable: "DATABASE_URL",
+		dialect: "postgres",
+		seederStorage: "sequelize",
+	},
 };
